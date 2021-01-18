8811bd40e388f887fde2d3a03430738ab7ec4a30
ph140 <petterh140@gmail.com>
2021-01-18 17:37:39 +0100

    fart for alle, med generell funksjon

--- opg1.py
+++ opg1.py
@@ -1,7 +1,13 @@
-import numpy as np
+# import numpy as np
 from matplotlib import pyplot as plt
 
+# Lager lister for farten
+liten_fart = [0]
+middels_fart = [0]
+stor_fart = [0]
 
+
+# Åpner og leser av filene
 with open('litenfallskjerm.txt', 'r') as file:
     liten_data = file.read()
 with open('middelsfallskjerm.txt', 'r') as file:
@@ -11,14 +17,24 @@ with open('storfallskjerm.txt', 'r') as file:
 
 
 def til_liste(data):
+    """
+    Fjerner uønsket tekst fra datasettet, og splitter opp i lister for å
+    skille akselerasjon og tid.
+    Returnerer en liste for tid, og en for akselerasjon.
+    """
     data = data.strip('Akselerasjonsverdiene er gitt ved ')
     data = data.replace('Verdiene for tid er gitt ved ', '')
     data = data.split('[')
-    return fiksdata(data[1]), fiksdata(data[2])
+    return datatrim(data[1]), datatrim(data[2])
 
 
-def fiksdata(liste):
-    liste = liste.split(',')
+def datatrim(data):
+    """
+    Lager en liste med alle verdiene, fjerner ']' fra det siste elementet.
+    Fjerner whitespace.
+    Returnerer en liste med floats.
+    """
+    liste = data.split(',')
     liste[-1] = liste[-1].replace(']', '')
 
     for index, item in enumerate(liste):
@@ -26,12 +42,26 @@ def fiksdata(liste):
     return liste
 
 
+def integral(datatid, datasett, x):
+    return (datatid[x+1]-datatid[x])*((datasett[x]+datasett[x+1]))/2
+
+
+def lagfart(tid, fart, akselerasjon):
+    for i, j in enumerate(tid[:-1]):
+        fart.append(fart[i-1]+integral(tid, akselerasjon, i))
+
+
+# Lager listene som trengs
 liten_akselerasjon, liten_tid = til_liste(liten_data)
 middels_akselerasjon, middels_tid = til_liste(middels_data)
 stor_akselerasjon, stor_tid = til_liste(stor_data)
 
 
-plt.plot(liten_tid, liten_akselerasjon, 'r-')
-plt.plot(middels_tid, middels_akselerasjon, 'b-')
-plt.plot(stor_tid, stor_akselerasjon, 'g-')
+lagfart(liten_tid, liten_fart, liten_akselerasjon)
+lagfart(middels_tid, middels_fart, middels_akselerasjon)
+lagfart(stor_tid, stor_fart, stor_akselerasjon)
+
+plt.plot(liten_tid, liten_fart, 'r-')
+plt.plot(middels_tid, middels_fart, 'b-')
+plt.plot(stor_tid, stor_fart, 'g-')
 plt.show()
