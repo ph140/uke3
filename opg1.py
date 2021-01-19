22325cd5c50cd6b1e25953285b1eb83575b39c47
ph140 <petterh140@gmail.com>
2021-01-19 22:40:21 +0100

    samme som forrige men litt bedre

--- opg1.py
+++ opg1.py
@@ -1,14 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Åpner og leser av filene
-with open('litenfallskjerm.txt', 'r') as file:
-    liten_data = file.read()
-with open('middelsfallskjerm.txt', 'r') as file:
-    middels_data = file.read()
-with open('storfallskjerm.txt', 'r') as file:
-    stor_data = file.read()
-
 
 def til_liste(data):
     """
@@ -82,26 +74,40 @@ def find_k(tid, fart, avvik=99999, k=0.01):
     return(k, v)
 
 
+# Åpner og leser av filene
+with open('litenfallskjerm.txt', 'r') as file:
+    liten_data = file.read()
+with open('middelsfallskjerm.txt', 'r') as file:
+    middels_data = file.read()
+with open('storfallskjerm.txt', 'r') as file:
+    stor_data = file.read()
+
 # Lager listene som trengs
 liten_akselerasjon, liten_tid = til_liste(liten_data)
 middels_akselerasjon, middels_tid = til_liste(middels_data)
 stor_akselerasjon, stor_tid = til_liste(stor_data)
 
+# Beregner fart
 liten_fart = lagfart(liten_tid, liten_akselerasjon)
 middels_fart = lagfart(middels_tid, middels_akselerasjon)
 stor_fart = lagfart(stor_tid, stor_akselerasjon)
 
-# Skriver akselerasjon go tid til filene
+# Beregner distanse
+liten_distanse = lagfart(liten_tid, liten_fart)
+middels_distanse = lagfart(middels_tid, middels_fart)
+stor_distanse = lagfart(stor_tid, stor_fart)
+
+# Lagrer akselerasjon og tid i filer
 skrivfil('liten_aks', liten_akselerasjon, liten_tid)
 skrivfil('middels_aks', middels_akselerasjon, middels_tid)
 skrivfil('stor_aks', stor_akselerasjon, stor_tid)
 
-# Beregner k-verdier
+# Beregner k-verdier, og farts-prognoser basert på eulers metode
 liten_k, v1 = find_k(liten_tid, liten_fart)
 middels_k, v2 = find_k(middels_tid, middels_fart)
 stor_k, v3 = find_k(stor_tid, stor_fart)
 
-
+# Radius-verdier oppgitt i oppgaven
 liten_radius = 0.11
 middels_radius = 0.17
 stor_radius = 0.33
@@ -116,14 +122,20 @@ stor_ratio = round(stor_areal/stor_k, 5)
 
 
 print(f'Liten_k: {liten_k}\nMiddels k: {middels_k}\nStor_k: {stor_k}\n\n\n')
-print(f'Liten: {liten_areal}\nMiddels: {middels_areal}\nStor: {stor_areal}\n\n')
-print(f'Liten: {liten_ratio}\nMiddels: {middels_ratio}\nStor: {stor_ratio}')
+print(f'Liten: {liten_areal}\nMiddels: {middels_areal}\nStor: {stor_areal}\n')
+print(f'Liten: {liten_ratio}\nMiddels: {middels_ratio}\nStor: {stor_ratio}\n')
+print(f'{liten_distanse[-1]}\n {middels_distanse[-1]}\n {stor_distanse[-1]}')
 
-
-plt.plot(liten_tid, v1, 'r--')
-plt.plot(liten_tid, liten_fart, 'r-')
+plt.plot(liten_tid, v1, 'r--', label='fart av prognose')
+plt.plot(liten_tid, liten_fart, 'r-', label='fart av data')
+plt.plot(liten_tid, liten_distanse, 'r:', label='distanse')
 plt.plot(middels_tid, v2, 'b--')
 plt.plot(middels_tid, middels_fart, 'b-')
+plt.plot(middels_tid, middels_distanse, 'b:')
 plt.plot(stor_tid, v3, 'g-')
 plt.plot(stor_tid, stor_fart, 'g-')
+plt.plot(stor_tid, stor_distanse, 'g:')
+plt.xlabel('Tid (s)')
+plt.ylabel('Fart (m/s)')
+plt.legend()
 plt.show()
